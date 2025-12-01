9ec547361dbdb9a04b6ff3e5680b2d9f82c5ea75
James <digjaya.james@gmail.com>
2025-12-01 22:29:04 +0800

    1

---
+++ Step01Data.py
@@ -0,0 +1,16 @@
+#
+# Step01Data, 2025/12/01
+# File: Step01Data.py
+# Short description of the task
+#
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 1. Input
+
+# 2. Process
+
+# 3. Output
