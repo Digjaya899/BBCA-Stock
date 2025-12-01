877ceb4513b6fcae6525de41195daf11f6f594d4
James <digjaya.james@gmail.com>
2025-12-02 01:10:52 +0800

    0

--- Step02Dimensionality.py
+++ Step02Dimensionality.py
@@ -8,10 +8,20 @@ import pandas as pd
 import statsmodels.api as sm
 import seaborn as sns
 import matplotlib.pyplot as plt
+import factor_analyzer as fa 
+from factor_analyzer import calculate_bartlett_sphericity
+from factor_analyzer import calculate_kmo
 
 # 1. Input
 df = pd.read_csv ('BBCA.csv') 
+df_numbers = df.select_dtypes(include='number')
 
-# 2. Process
+# 2. Bartlett
+p_value= calculate_bartlett_sphericity(df_numbers) 
+print(f"Bartlett's Test p value = {p_value}")
 
-# 3. Output
+# 3. KMO
+kmo_model = calculate_kmo(df_numbers)
+print(f"KMO = {kmo_model}")
+
+#FAIL!
