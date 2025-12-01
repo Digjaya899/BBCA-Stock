a19a21619285b02e371bc6e886fe259f9a399245
James <digjaya.james@gmail.com>
2025-12-02 00:29:35 +0800

    8

---
+++ Step02Dimensionality.py
@@ -0,0 +1,17 @@
+#
+# Step02, 2025/12/02
+# File: Step02.py
+# Short description of the task
+#
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 1. Input
+df = pd.read_csv ('BBCA.csv') 
+
+# 2. Process
+
+# 3. Output
