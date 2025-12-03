c4df33fd133c29c1dbaafc07656775fcb2b27f8c
James <digjaya.james@gmail.com>
2025-12-04 03:58:24 +0800

    15

--- Step03_CorrelationCausation.py
+++ Step03_CorrelationCausation.py
@@ -3,8 +3,22 @@
 # File: Step03_CorrelationCausation.py
 # Short description of the task
 #
+import pandas as pd
 
-# 1. Input
+# 3.1 Purpose
+import pandas as pd
+
+if "df" not in globals():
+    df = pd.read_csv("BBCA.csv")
+
+target_corr = (
+    df.corr(numeric_only=True)["Stock_Price_Rp"]
+      .drop("Stock_Price_Rp").drop("Year")
+      .sort_values(ascending=False)
+      .head(5)
+)
+
+print(target_corr.round(3))
 
 # 2. Process
 
