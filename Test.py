931fc463f4b814e30081d144e07c46209edc0ca6
James <digjaya.james@gmail.com>
2025-12-05 00:31:42 +0800

    19

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 #
-# Test, 2025/12/02
+# Test, 2025/12/05
 # File: Test.py
 # Short description of the task
 #
@@ -9,6 +9,7 @@ import statsmodels.api as sm
 import seaborn as sns
 import matplotlib.pyplot as plt 
 from scipy import stats 
+from sklearn.decomposition import FactorAnalysis
 
 # 1. Input
 if "df" not in globals():
@@ -16,35 +17,18 @@ if "df" not in globals():
 
 z_scores = np.abs(
     stats.zscore(
-        df [["Stock Price (Rp)","EPS (Rp)","Dividends (Rp)",
-             "P/E","ROA (%)","ROE (%)","Debt-to-Equity",]]
-            .dropna()
-        )
-    )
+        df [["Year","Stock_Price_Rp","Highest_Stock_Price_Rp","Lowest_Stock_Price_Rp","Dividends_Rp",
+               "EPS_Rp","PE","ROA_pct","ROE_pct","Debt_to_Equity",
+               "Total_Assets_BnRp","Total_Liabilities_BnRp","Total_Debt_BnRp","Total_Equity_BnRp",
+               "Revenue_BnRp","EBITDA_BnRp","Net_Profit_BnRp","Operating_Income_BnRp","Operating_Cost_BnRp",]
+               ].dropna()))
 
 outlier_mask = z_scores > 3
 
 outliers = df.loc[outlier_mask.any(axis=1),
-                  ["Year","Stock Price (Rp)","EPS (Rp)","Dividends (Rp)",
-                   "P/E","ROA (%)","ROE (%)","Debt-to-Equity",],
-                   ]
+                  ["Year","Stock_Price_Rp","Highest_Stock_Price_Rp","Lowest_Stock_Price_Rp","Dividends_Rp",
+                   "EPS_Rp","PE","ROA_pct","ROE_pct","Debt_to_Equity",
+                   "Total_Assets_BnRp","Total_Liabilities_BnRp","Total_Debt_BnRp","Total_Equity_BnRp",
+                   "Revenue_BnRp","EBITDA_BnRp","Net_Profit_BnRp","Operating_Income_BnRp","Operating_Cost_BnRp",],]
 
 print(outliers)
-
-# 2. Process
-
-# Step02 MetaData
-
-# if "df" not in globals():
-#     df = pd.read_csv("BBCA.csv")
-
-# exp_cols = ["Year","Stock Price (Rp)","EPS (Rp)","Dividends (Rp)",
-#      "P/E","ROA (%)","ROE (%)","Debt-to-Equity",
-#      "Total Assets (Rp)","Total Liabilities (Rp)","Total Debt (Rp)","Total Equity (Rp)",
-#      "Revenue (Rp)","Net Profit (Rp)","Operating Cash Flow (Rp)",]
-
-
-# ["Year","Stock_Price_Rp","EPS_Rp","Dividends_Rp",
-# "PE","ROA_pct","ROE_pct","Debt_to_Equity",
-# "Total_Assets_BnRp","Total_Liabilities_BnRp","Total_Debt_BnRp","Total_Equity_BnRp",
-# "Revenue_BnRp","Net_Profit_BnRp","Operating_Cash_Flow_BnRp",]
